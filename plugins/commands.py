9ead5aa03d067d7e9ea32dbdd14c7aa093b41b5e
shammasikko <shemmas1520@gmail.com>
2024-11-30 11:47:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -652,7 +652,7 @@ async def start(client, message):
             pass
         return await message.reply('No such file exist.')
     files = files_
-    title = '@VJ_Botz  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files["file_name"].split()))
+    title = '  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files["file_name"].split()))
     size=get_size(files["file_size"])
     f_caption=files["caption"]
     if CUSTOM_FILE_CAPTION:
